1064518435ca405790303cf2f399c31a613bc6e8
rigdoge <rigdoge@users.noreply.github.com>
2025-02-03 17:15:02 +0800

    v0.3.0: Enhance mobile responsiveness
    - Optimize card width and layout for mobile devices
    - Center artist cards on mobile screens
    - Adjust card size and spacing for better readability
    - Set max-width for better visual consistency

--- globe-artists/dev-dist/sw.js
+++ globe-artists/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-99d8380f'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.rquaupc5clo"
+    "revision": "0.lisogamvdp8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
