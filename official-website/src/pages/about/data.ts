85fa1f6808784c2af4b67350cc4149a538179c60
rigdoge <rigdoge@users.noreply.github.com>
2025-02-08 21:15:09 +0800

    chore: update aliexpress link to official website

--- official-website/src/pages/about/data.ts
+++ official-website/src/pages/about/data.ts
@@ -1,17 +1,30 @@
 export interface Section {
-  image: string | React.ComponentType<React.SVGProps<SVGSVGElement>>;
   title: string;
-  subtitle?: string;
+  subtitle: string;
   description: string;
-  dark?: boolean;
+  icons: React.ComponentType<React.SVGProps<SVGSVGElement>>[];
+  socialLinks: {
+    aliexpress?: string;
+    facebook?: string;
+    instagram?: string;
+  };
 }
 
-export const sections: Section[] = [
-  {
-    image: require('@site/src/assets/tattoo-icons.svg').default,
-    title: 'ABOUT US',
-    subtitle: 'AT EZ TATTOO, WE ARE MORE THAN JUST A TATTOO SUPPLY COMPANY',
-    description: 'We are a driving force in the world of tattoo. Our passion for innovation, commitment to quality, and dedication to our community have made us a trusted name in the industry.',
-    dark: true
+export const aboutSection: Section = {
+  title: 'ABOUT US',
+  subtitle: 'AT EZ TATTOO, WE ARE MORE THAN JUST A TATTOO SUPPLY COMPANY',
+  description: 'We are a driving force in the world of tattoo. Our passion for innovation, commitment to quality, and dedication to our community have made us a trusted name in the industry.',
+  icons: [
+    require('@site/src/assets/icons/tattoo-machine.svg').default,
+    require('@site/src/assets/icons/ink-bottle.svg').default,
+    require('@site/src/assets/icons/power-supply.svg').default,
+    require('@site/src/assets/icons/cartridge.svg').default,
+    require('@site/src/assets/icons/grip.svg').default,
+    require('@site/src/assets/icons/tape.svg').default,
+  ],
+  socialLinks: {
+    aliexpress: 'https://eztattoosupply.com',
+    facebook: 'https://www.facebook.com/eztattoosupply',
+    instagram: 'https://www.instagram.com/eztattoosupply'
   }
-]; 
+}; 
